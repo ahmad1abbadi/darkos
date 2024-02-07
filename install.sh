afcbff472cffc006a8fc3cb0ec49bb8e2bd37b5b
ahmad1abbadi <156505848+ahmad1abbadi@users.noreply.github.com>
2024-02-07 21:43:08 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ apt-get update &>/dev/null
 apt-get -y --with-new-pkgs -o Dpkg::Options::="--force-confdef" upgrade &>/dev/null
 apt install python --no-install-recommends -y &>/dev/null
 clear
-curl -o native.py https://raw.githubusercontent.com/ahmad1abbadi/darkos/blob/main/installglibc.py && python3 installglipc.py
+curl -o installglibc.py https://raw.githubusercontent.com/ahmad1abbadi/darkos/blob/main/installglibc.py && python3 installglibc.py
 fi
