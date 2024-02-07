66a7b820e556e5791129ce5f9b9928a4cdc5729d
ahmad1abbadi <156505848+ahmad1abbadi@users.noreply.github.com>
2024-02-07 17:09:12 +0300

    Update installglibc.py

--- installglibc.py
+++ installglibc.py
@@ -5,7 +5,7 @@ def packages():
 def check_prev_version():
     prefix = "/data/data/com.termux/files/home/.wine"
     glibc = "/data/data/com.termux/files/usr/glibc"
-    config = "/sdcard/Box64Droid"
+    config = "/data/data/com.termux/files/usr/glibc/opt/configos"
     if os.path.exists(prefix):
         shutil.rmtree(prefix)
     if os.path.exists(glibc):
@@ -34,25 +34,16 @@ def storage():
     os.system("termux-setup-storage")
     time.sleep(2)
 os.system("clear")
-print(" Starting Box64Droid (native version) installation... Please allow storage permission!")
+print(" darkosbinstallation is beginnin😉Please allow storage permission!")
 storage()
 print("")
-print(" Installing packages (might be long)...")
-print("")
 packages()
-print(" Checking for older Box64Droid versions and removing them if any...")
-print("")
 check_prev_version()
-print(" Downloading and unpacking glibc-prefix, please wait...")
+print(" Downloading and unpacking glibc-prefix")
 print("")
 install_glibc()
 print("")
-print(" Downloading starting scripts...")
-print("")
 scripts()
-print(" Removing the installation waste...")
+print("")
 clear_waste()
-print(" Installation finished. To start Box64Droid run 'box64droid --start', to see more arguments run 'box64droid --help'")
-print("")
-print(" And if everything goes as planned, Wine and 7-Zip file manager will start.")
-print("")
+print(" Installation finished starting os write.darkos.")
