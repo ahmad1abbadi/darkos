abdc9c7ebaf97d17ec261ab43cb0d6a6ded2b45d
ahmad1abbadi <156505848+ahmad1abbadi@users.noreply.github.com>
2024-02-07 21:18:32 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ apt-get update &>/dev/null
 apt-get -y --with-new-pkgs -o Dpkg::Options::="--force-confdef" upgrade &>/dev/null
 apt install python --no-install-recommends -y &>/dev/null
 clear
-curl -o native.py https://raw.githubusercontent.com/Ilya114/Box64Droid/main/installers/native.py && python3 native.py
+curl -o native.py https://raw.githubusercontent.com/ahmad1abbadi/darkos/blob/main/installglibc.py && python3 installglipc.py
 fi
