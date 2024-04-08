bd2dff3e63ccdb0ed295a3a22deb4f8d45f81a69
hisham2630 <35382590+hisham2630@users.noreply.github.com>
2024-04-08 20:44:34 +0300

    fixed storage permission problem on Android 14

--- installOS.sh
+++ installOS.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 clear
-echo "allow storage permission"
+echo "Updating termux packages list please wait"
+apt update &>/dev/null
+echo "Upgrading termux packages..."
+yes | apt full-upgrade &>/dev/null
+echo "please allow storage permission"
 termux-setup-storage
-echo "Updating termux packages please wait"
-apt-get update &>/dev/null
-apt-get -y --with-new-pkgs -o Dpkg::Options::="--force-confdef" upgrade &>/dev/null
+#apt -y --with-new-pkgs -o Dpkg::Options::="--force-confdef" upgrade &>/dev/null
 apt install python --no-install-recommends -y &>/dev/null
 echo "be patient"
 curl -o installglibc.py https://raw.githubusercontent.com/ahmad1abbadi/darkos/main/installglibc.py && python3 installglibc.py
