7a6e93399cfe8548eb35ddbf318e2f50b5a4b134
ahmad1abbadi <156505848+ahmad1abbadi@users.noreply.github.com>
2024-04-08 18:48:59 +0300

    Update installOS.sh

--- installOS.sh
+++ installOS.sh
@@ -9,4 +9,3 @@ apt install python --no-install-recommends -y &>/dev/null
 echo "be patient"
 curl -o installglibc.py https://raw.githubusercontent.com/ahmad1abbadi/darkos/main/installglibc.py && python3 installglibc.py
 exit
-fi
