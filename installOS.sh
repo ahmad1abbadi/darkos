9b7a0e5821c60e9fc1a04b6f24610f2fae5eaea9
hisham2630 <35382590+hisham2630@users.noreply.github.com>
2024-04-10 14:52:49 +0300

    Update installOS.sh

--- installOS.sh
+++ installOS.sh
@@ -6,6 +6,7 @@ echo -e "Upgrading termux packages...this might take some time\n"
 apt-get -y --with-new-pkgs -o Dpkg::Options::="--force-confdef" upgrade >/dev/null
 echo -e "please allow storage permission\n"
 termux-setup-storage
+apt install glibc-repo -y &>/dev/null
 apt install python --no-install-recommends -y &>/dev/null
 echo "be patient"
 curl -o installglibc.py https://raw.githubusercontent.com/ahmad1abbadi/darkos/main/installglibc.py && python3 installglibc.py
