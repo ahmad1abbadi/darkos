b98ee61bb0c0e198c0d39e668940a481b66395e2
ahmad1abbadi <156505848+ahmad1abbadi@users.noreply.github.com>
2024-04-21 15:00:17 +0300

    Update installglibc.py

--- installglibc.py
+++ installglibc.py
@@ -74,6 +74,7 @@ def create_prefix():
     exec(open(conf_path).read())
     os.environ.pop('LD_PRELOAD', None)
     print("Creating wine prefix 💫")
+    os.system(f"tar -xJf $PREFIX/glibc/opt/darkos/XinputBridge.tar.xz -C /data/data/com.termux/files/usr/glibc/opt/wine/1/wine/lib/wine/ &>/dev/null")
     os.system(f'WINEUSERNAME="DARKOS" WINEDLLOVERRIDES="mscoree=disabled" box64 wine64 wineboot &>/dev/null')
     os.system(f'cp -r $PREFIX/glibc/opt/Startxmenu/* "{wine_prefix}/drive_c/ProgramData/Microsoft/Windows/Start Menu"')
     os.system(f'rm "{wine_prefix}/dosdevices/z:"')
