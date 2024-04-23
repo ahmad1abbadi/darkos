202df60c35918c1240f25d5a538ede964c81b1c2
ahmad1abbadi <156505848+ahmad1abbadi@users.noreply.github.com>
2024-04-23 13:14:21 +0300

    Update installOS.sh

--- installOS.sh
+++ installOS.sh
@@ -2,13 +2,12 @@
 clear
 echo -e "Updating termux packages list please wait\n"
 apt update &>/dev/null
+echo -e "Upgrading termux packages...this might take some time\n"
 yes | apt-get full-upgrade &>/dev/null
 unlink "$PREFIX/etc/termux/chosen_mirrors" &>/dev/null
 ln -s "$PREFIX/etc/termux/mirrors/all" "$PREFIX/etc/termux/chosen_mirrors" &>/dev/null
 echo -e "Selecting best termux packages mirror please wait\n"
 TERMUX_APP_PACKAGE_MANAGER=apt pkg --check-mirror update
-echo -e "Upgrading termux packages...this might take some time\n"
-apt-get -y --with-new-pkgs -o Dpkg::Options::="--force-confdef" upgrade >/dev/null
 echo -e "please allow storage permission\n"
 termux-setup-storage
 apt install python --no-install-recommends -y &>/dev/null
