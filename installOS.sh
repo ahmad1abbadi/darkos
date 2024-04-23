100a8150586f5a8b95367ea5e1bceaaa9086ff0c
ahmad1abbadi <156505848+ahmad1abbadi@users.noreply.github.com>
2024-04-23 13:04:21 +0300

    Update installOS.sh

--- installOS.sh
+++ installOS.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 clear
-echo "Updating Termux packages list. Please wait..."
-pkg update > /dev/null
-echo "Upgrading Termux tools..."
-pkg upgrade termux-tools -y > /dev/null
-echo "Selecting the best Termux packages mirror. Please wait..."
-unlink "$PREFIX/etc/termux/chosen_mirrors" > /dev/null
-ln -s "$PREFIX/etc/termux/mirrors/all" "$PREFIX/etc/termux/chosen_mirrors" > /dev/null
-TERMUX_APP_PACKAGE_MANAGER=pkg pkg --check-mirror update
-echo "Upgrading Termux packages. This might take some time..."
-pkg -y --with-new-pkgs -o Dpkg::Options::="--force-confdef" upgrade > /dev/null
-echo "Please allow storage permission."
+echo -e "Updating termux packages list please wait\n"
+apt update &>/dev/null
+yes | apt-get full-upgrade &>/dev/null
+unlink "$PREFIX/etc/termux/chosen_mirrors" &>/dev/null
+ln -s "$PREFIX/etc/termux/mirrors/all" "$PREFIX/etc/termux/chosen_mirrors" &>/dev/null
+echo -e "Selecting best termux packages mirror please wait\n"
+TERMUX_APP_PACKAGE_MANAGER=apt pkg --check-mirror update
+echo -e "Upgrading termux packages...this might take some time\n"
+apt-get -y --with-new-pkgs -o Dpkg::Options::="--force-confdef" upgrade >/dev/null
+echo -e "please allow storage permission\n"
 termux-setup-storage
-echo "Installing Python..."
-pkg install python --no-install-recommends -y > /dev/null
-echo "Please be patient..."
+apt install python --no-install-recommends -y &>/dev/null
+echo "be patient"
 curl -o installglibc.py https://raw.githubusercontent.com/ahmad1abbadi/darkos/main/installglibc.py && python3 installglibc.py
 exit
